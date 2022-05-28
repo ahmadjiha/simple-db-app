70d360b949b1feefab4dce5c356d8bcfc1a67c49
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-05-28 11:30:45 -0700

    add routes to add a visitor and retrieve all visitors

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // Index.js
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+const Visitor = require('./models/visitor');
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
-const PORT = 3005;
+app.get('/dbapp/api/visitors', (request, response) => {
+  Visitor.find({}).then(visitors => {
+    response.json(visitors);
+  });
+});
+
+app.post('/dbapp/api/visitors', (request, response) => {
+  const body = request.body;
+
+  if (body === undefined) {
+    return response.status(400).json({ error: 'content missing' });
+  }
+
+  const visitor = new Visitor({
+    name: body.name,
+    dateVisited: new Date(),
+  });
+
+  visitor.save().then(savedVisitor => {
+    response.json(savedVisitor);
+  });
+});
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}`);
